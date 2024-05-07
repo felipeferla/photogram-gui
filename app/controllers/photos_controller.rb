a178b5b86fba1ab19fffdda1c99832f5875f756d
felipeferla <164975883+felipeferla@users.noreply.github.com>
2024-05-07 21:17:04 +0000

    v10

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -20,4 +20,16 @@ class PhotosController < ApplicationController
 
     redirect_to("/photos")
   end
+
+  def create
+    new_photo = Photo.new
+    new_photo.image = params.fetch("query_image")
+    new_photo.caption = params.fetch("query_caption")
+    new_photo.owner_id = params.fetch("query_owner_id")
+    new_photo.save
+   # render({:template => "photo_templates/create"})
+   redirect_to("/photos/" + new_photo.id.to_s )
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
+  get("/users", { :controller => "users", :action => "index" })
 
-get("/users", { :controller => "users", :action => "index"})
+  get("/users/:path_username", { :controller => "users", :action => "show" })
 
-get("/users/:path_username", { :controller => "users", :action => "show"})
+  get("/photos", { :controller => "photos", :action => "index" })
 
-get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
 
-get("/photos/:path_id", { :controller => "photos", :action => "show"})
+  get("/delete_photo/:photo_id", { :controller => "photos", :action => "baii" })
 
-get("/delete_photo/:photo_id", { :controller => "photos", :action => "baii"})
-
-
+  get("/insert_photo", { :controller => "photos", :action => "create" })
 end
