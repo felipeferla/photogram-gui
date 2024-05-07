924edf595bf0e614c070b74bb90c36c98ada969b
felipeferla <164975883+felipeferla@users.noreply.github.com>
2024-05-07 19:16:50 +0000

    v7

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,14 @@
 class PhotosController < ApplicationController
   def index
-    @list_of_photos = Photo.all.order({:created_at => :desc})
+    @list_of_photos = Photo.all.order({ :created_at => :desc })
 
     render({ :template => "photo_templates/index" })
+  end
+
+  def show
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => url_id })
+    @the_photo = matching_photos.first
+    render ({ :template => "photo_templates/show" })
   end
 end
